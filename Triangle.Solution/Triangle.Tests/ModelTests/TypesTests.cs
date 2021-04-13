9c2eb2d1fb21ee73adef74e6c92099a4c305295c
Garrett Brown <garrettpaulbrown@gmail.com>
2021-04-12 19:05:02 -0700

    Add test and models.
    Co-authored-by: Laurie Waller <lauriewaller7@gmail.com>

---
+++ Triangle.Solution/Triangle.Tests/ModelTests/TypesTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangle;
+
+namespace Triangle.Tests
+{
+  // [TestClass]
+  // // public class TypesTests
+  // // // {
+  // // //   [TestMethod]
+  // // //   // public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+  // // //   // {
+  // // //   //   // any necessary logic to prep for test; instantiating new classes, etc.
+  // // //   //   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+  // // //   // }
+  // // // }
+}
